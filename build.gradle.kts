4b409c92fd9ef57568714c7877a78508779f989c
cyberc3dr <geo3gamer@gmail.com>
2026-09-08 12:54:57 +0300

    pclabs: init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    application
+    alias(libs.plugins.shadow)
+}
+
+application {
+    mainClass = "ru.cyberc3dr.pc.Main"
+}
+
+group = "ru.cyberc3dr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release = 21
+}
+
+tasks.withType<Jar> {
+    destinationDirectory = file("$rootDir/build")
+    archiveVersion = ""
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+}
+
+sourceSets.main {
+    java.srcDir("src")
+    resources.srcDir("resources")
+}
---
+++ src/ru/cyberc3dr/pc/Main.java
@@ -0,0 +1,8 @@
+package ru.cyberc3dr.pc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
